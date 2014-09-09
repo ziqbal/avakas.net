88fac71f52fae7906d6ec7fa1f1571ba11716206
Zafar Iqbal <ultrasine@gmail.com>
2014-09-09 18:12:44 +0300

    added dynamic images, seperator

---
+++ _teacms_/_stars.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+$__stars[]=array(0,"markers/page.png","Page",1,array("maintitle","shortdescription","longdescription"));
+
+$__stars[]=array(100,"markers/star-2.png","Text",1,array("maintitle","shortdescription","longdescription"));
+$__stars[]=array(101,"markers/star-3.png","Image",1,array("maintitle","fuid"));
+$__stars[]=array(102,"markers/star-4.png","Video",1,array("maintitle","embed"));
+
+$__stars[]=array(105,"markers/star-7.png","File",1,array("maintitle","fuid"));
+
+$__stars[]=array(201,"markers/circle-1.png","Page Banner",1,array("maintitle","fuid")); 
+$__stars[]=array(202,"markers/circle-2.png","Page Ad",1,array("maintitle","fuid","link")); 
+
+
+$__stars[]=array(203,"markers/circle-3.png","Homepage Banner",1,array("maintitle","fuid")); 
+$__stars[]=array(204,"markers/circle-4.png","Homepage Side",1,array("maintitle","fuid")); 
+
+
+
