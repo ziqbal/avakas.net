8c3c067ca1b77519b9790ab91ec2abc4858a0211
Zafar Iqbal <ultrasine@gmail.com>
2014-10-14 18:36:37 +0300

    dynamic contact, iframe embed, external links

--- _teacms_/_stars.php
+++ _teacms_/_stars.php
@@ -9,18 +9,22 @@ $__stars[]=array(101,"markers/star-3.png","Image",1,array("maintitle","fuid"));
 $__stars[]=array(102,"markers/star-4.png","Video",1,array("maintitle","embed"));
 
 $__stars[]=array(105,"markers/star-7.png","File",1,array("maintitle","fuid"));
+$__stars[]=array(106,"markers/star-8.png","Embed",1,array("maintitle","embed"));
 
 $__stars[]=array(201,"markers/circle-1.png","Page Banner",1,array("maintitle","fuid")); 
 $__stars[]=array(202,"markers/circle-2.png","Page Ad",1,array("maintitle","fuid","link")); 
 
 
 $__stars[]=array(203,"markers/circle-3.png","Homepage Banner",1,array("maintitle","fuid")); 
-$__stars[]=array(204,"markers/circle-4.png","Homepage Side",1,array("maintitle","fuid")); 
+$__stars[]=array(204,"markers/circle-4.png","Homepage Side",1,array("maintitle","fuid","link")); 
 $__stars[]=array(205,"markers/circle-5.png","Homepage Left Nav",1,array("maintitle","link")); 
 
 $__stars[]=array(301,"markers/square-1.png","Top Text",1,array("maintitle")); 
 $__stars[]=array(302,"markers/square-2.png","Homepage Quote Text",1,array("maintitle","shortdescription")); 
 $__stars[]=array(303,"markers/square-3.png","Page Quote Text",1,array("maintitle","shortdescription")); 
+
+
+$__stars[]=array(9002,"markers/diamond-1.png","Contact",1,array("maintitle"));
 
 
 
